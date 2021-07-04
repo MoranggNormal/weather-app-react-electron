7c297372ee18935b636713c8c2a04dcbb5ba3650
euller <eullerz01@hotmail.com>
2021-07-04 05:56:26 -0300

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactWeather, { useOpenWeather } from 'react-open-weather';
+
 
 function App() {
+
+  const { data, isLoading, errorMessage } = useOpenWeather({
+  key: '24479dce2a5b7d028a46e6d77cac6006',
+  lat: '1.2921',
+  lon: '36.8219',
+  lang: 'en',
+  unit: 'metric', // values are (metric, standard, imperial)
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ReactWeather
+    isLoading={isLoading}
+    errorMessage={errorMessage}
+    data={data}
+    lang="en"
+    locationLabel="Ipojuca"
+    unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+    showForecast
+  />
+</div>
+
   );
 }
 
